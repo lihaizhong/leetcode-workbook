9442a65e2078706772a77d167b32d5493109c3bc
sky <854323752@qq.com>
2022-03-12 14:17:49 +0800

    chore: 搭建项目

---
+++ 1.两数之和.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2022 lihz
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+/*
+ * @lc app=leetcode.cn id=1 lang=typescript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  return [];
+}
+// @lc code=end
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+<!--
+ Copyright (c) 2022 Sangbaipi
+ 
+ This software is released under the MIT License.
+ https://opensource.org/licenses/MIT
+-->
+
 # LeetCode Workbook
