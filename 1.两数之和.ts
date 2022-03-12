29f051a43fbf6b0368b815cd49bb03faeb30b3b2
sky <854323752@qq.com>
2022-03-13 04:01:50 +0800

    feat: 完成两数之和

--- 1.两数之和.ts
+++ 1.两数之和.ts
@@ -6,6 +6,19 @@
 
 // @lc code=start
 function twoSum(nums: number[], target: number): number[] {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const num1 = nums[i];
+    const num2 = target - num1;
+
+    if (map.has(num2)) {
+      return [map.get(num2), i];
+    }
+
+    map.set(num1, i);
+  }
+
   return [];
 }
 // @lc code=end
