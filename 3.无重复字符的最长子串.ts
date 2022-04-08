942060bfcd22dc4b8c7c8b26f8759e8df65e4f8f
sky <854323752@qq.com>
2022-04-08 11:36:17 +0800

    fix: 更新 无重复字符的最长子串

--- 3.无重复字符的最长子串.ts
+++ 3.无重复字符的最长子串.ts
@@ -13,17 +13,16 @@ function lengthOfLongestSubstring(s: string): number {
   // const 与 let性能相差比较大
   const saver: Set<string> = new Set();
   const size: number = s.length;
-  let rk: number = -1;
+  let rk: number = 0;
   let max: number = 0;
 
-  for (let lk = 0; lk < s.length; lk++) {
+  for (let lk = 0; lk < s.length && rk < size; lk++) {
     if (lk !== 0) {
       saver.delete(s.charAt(lk - 1));
     }
 
-    while (rk < size && !saver.has(s.charAt(rk + 1))) {
-      saver.add(s.charAt(rk + 1));
-      ++rk;
+    while (rk < size && !saver.has(s.charAt(rk))) {
+      saver.add(s.charAt(rk++));
     }
 
     max = Math.max(saver.size, max);
