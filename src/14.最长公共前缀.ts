afddaf6b1d8094609aaa5b2ac49defe30153987e
sangbaipi <sangbaipi@2dfire.com>
2023-04-12 10:23:43 +0800

    fix: 添加设计模式

---
+++ src/14.最长公共前缀.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=typescript
+ *
+ * [14] 最长公共前缀
+ */
+
+// @lc code=start
+function longestCommonPrefix(strs: string[]): string {
+  let prefix: string = ''
+
+  for (let i = 0; i < strs[0].length; i++) {
+    
+  }
+
+  return prefix
+};
+// @lc code=end
+
