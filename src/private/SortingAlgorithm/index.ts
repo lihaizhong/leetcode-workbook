0174dd35606df722e2c413ab212366a7d3b78f52
sky <854323752@qq.com>
2025-01-04 09:36:37 +0800

    feat: 排序算法

--- src/private/SortingAlgorithm/index.ts
+++ src/private/SortingAlgorithm/index.ts
@@ -5,14 +5,47 @@
 
 /**
  * 冒泡排序<交换排序>
+ * @param arr
  */
-export function bubbleSort() {
+// #region bubble-sort
+export function bubbleSort(arr: number[]): void {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      // 如果前一个元素大于后一个元素，则交换
+      if (arr[j] > arr[j + 1]) {
+        const tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
 }
+// #endregion bubble-sort
 
 /**
  * 快速排序<交换排序>
+ * @param arr
  */
-export function quickSort() {}
+// #region quick-sort
+export function quickSort(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex: number = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      // 如果当前最小的元素大于比较的元素，则交换
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      const tmp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = tmp;
+    }
+  }
+}
+// #endregion quick-sort
 
 /**
  * 简单选择排序<选择排序>
